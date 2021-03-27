714cd94098002a7a10bf517a6811cd2a7215dfc4
Chris Stewart <cstewart@techie.com>
2021-03-26 19:50:34 -0600

    Fixed floating point error

--- js/script.js
+++ js/script.js
@@ -1,22 +1,17 @@
-// let numbers = [10,20,30,40,50,60,70,80,90];
+let numbers = [1,2,3,4,5,6,7,8,9];
 let combos = [];
-// let emptycombo = [];
-// let total = 27;
+let emptycombo = [];
+let total = 27;
 
 let sum = function(nums) {
     return nums.reduce((accumulator, currentValue) => accumulator + currentValue);
 }
 
-let combinations = function (combination, choices, target){
-    let combinationSum = 0;
-    if (combination.length > 0) {
-        combinationSum = sum(combination);
-    } 
-    if (combinationSum == target) {
-        combos.push(combination);
-        return;
-    }
-    if (combinationSum > target) {
+let combinations = function (combination, choices, n){
+    if (combination.length == n) {
+        if (total == sum(combination)) {
+            combos.push(combination);
+        }
         return;
     }
     for (let i = 0; i < choices.length; i++) {
@@ -24,31 +19,12 @@ let combinations = function (combination, choices, target){
         newCombination.push(choices[i]);
         let newChoices = [...choices];
         newChoices.splice(0, i+1);
-        combinations(newCombination, newChoices, target);
+        combinations(newCombination, newChoices, n);
     }
 }
 
 // combinations(emptycombo, numbers, 6);
-// combinations([], numbers, 130);
-// console.log(combos);
+combinations([], numbers, 6);
 
 
-function Geeks() {
-    let input = document.getElementById('inputarray').value;
-    let targetnumber = document.getElementById('targetnumber').value;
-    console.log(input);
-    console.log(targetnumber);
-    combos = [];
-    let numbers = input.split(',').map(Number);
-    let target = 0+targetnumber;
-    combinations([], numbers, targetnumber);
-
-    let textOutput = "";
-
-    for (let i = 0; i < combos.length; i++) {
-        textOutput += combos[i].toString().split(",");
-        textOutput += "<br>"        
-    }
-    
-    document.getElementById("par").innerHTML = textOutput;
-}
+console.log(combos);
